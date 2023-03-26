ec806a5410962d85de838387ff161e23ca8dc301
ivnvtosh <ivnvtosh@gmail.com>
2023-03-26 23:29:29 +0300

    Refactor Presenter from 1

--- Main/Presenter.swift
+++ Main/Presenter.swift
@@ -15,6 +15,8 @@ final class Presenter {
 	
 	var timeInterval = 10.0
 	
+	var isCurrencyTracked = false
+	
 	init(interactor: Interactor) {
 		
 		self.interactor = interactor
@@ -22,32 +24,7 @@ final class Presenter {
 	
 	func didLoad() {
 		
-		startTimer()
 		loadCurrencies()
-	}
-	
-	func startTimer() {
-		
-		let timer = Timer(timeInterval: timeInterval,
-						  target: self,
-						  selector: #selector(refresh),
-						  userInfo: nil,
-						  repeats: true)
-		
-		RunLoop.current.add(timer, forMode: .common)
-		
-		self.timer = timer
-	}
-	
-	@objc func refresh() {
-		
-		loadCurrencies()
-	}
-	
-	func cancelTimer() {
-		
-		timer?.invalidate()
-		timer = nil
 	}
 	
 	func loadCurrencies() {
@@ -58,6 +35,10 @@ final class Presenter {
 				
 				let rates = try await interactor.load(currencies: "RUB, EUR, BDT",
 													  baseCurrency: "USD")
+				if isCurrencyTracked == false {
+					
+					startCurrencyTracked()
+				}
 				
 				print(rates)
 				// view?.show(rates)
@@ -65,11 +46,39 @@ final class Presenter {
 			
 			catch {
 				
-				cancelTimer()
+				cancelCurrencyTracked()
 				
 				print(error)
-				// router.showAlert(with: error, and: startTimer)
+				// router.showAlert(with: error, andTryAgain: loadCurrencies)
 			}
 		}
+	}
+	
+	func startCurrencyTracked() {
+		
+		isCurrencyTracked = true
+		
+		let timer = Timer(timeInterval: timeInterval,
+						  target: self,
+						  selector: #selector(refreshCurrency),
+						  userInfo: nil,
+						  repeats: true)
+		
+		RunLoop.current.add(timer, forMode: .common)
+		
+		self.timer = timer
+	}
+	
+	@objc func refreshCurrency() {
+		
+		loadCurrencies()
+	}
+	
+	func cancelCurrencyTracked() {
+		
+		isCurrencyTracked = false
+		
+		timer?.invalidate()
+		timer = nil
 	}
 }
